5c935e195cb107ba009a100dc273f6665cdf9cba
BeruzDev <albertus1291@gmail.com>
2025-05-23 18:54:12 +0200

    ...props on button component

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Button.css'
 
-const button = ({ Icon, onClick, className, children }) => {
+const button = ({ Icon, onClick, className, children, ...props }) => {
   return (
-    <button type="button" onClick={onClick} className={`button ${className}`}>
+    <button type="button" onClick={onClick} className={`button ${className}`} {...props}>
       {Icon && <Icon className="icon"/>}
       {children && <span className='button-text'>{children}</span>}
     </button>
